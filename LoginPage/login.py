d16eab8d2b02b228fa88269dae09ff6f680c07eb
ChristTech <86727542+ChristTech@users.noreply.github.com>
2024-01-22 15:47:12 +0100

    22\01\2024

---
+++ LoginPage/login.py
@@ -0,0 +1,54 @@
+import os
+os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+
+from kivy.animation import Animation
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+Window.size = (305, 480)
+
+
+class LoginApp(MDApp):
+
+    def change_screen(self, name):
+        # whatever argument will be passed in as name, it will change the screen
+        screen_manager.current = name
+
+    def build(self):
+        global screen_manager
+        # controls the separate screens
+        screen_manager = ScreenManager()
+        # this loads the specified .kv file to be used compared to just returning it from the build file
+        screen_manager.add_widget(Builder.load_file("login.kv"))
+        screen_manager.add_widget(Builder.load_file("admin_login.kv"))
+        return screen_manager
+
+    def anim(self, widget):
+        anim = Animation(pos_hint = {"center_y": 1.16})
+        anim.start(widget)
+
+    def anim1(self, widget):
+        anim = Animation(pos_hint = {"center_y": .85})
+        anim.start(widget)
+
+    def icons(self, widget):
+        anim = Animation(pos_hint={"center_y": .8})
+        anim += Animation(pos_hint={"centre_y": .85})
+        anim.start(widget)
+
+    def text(self, widget):
+        anim = Animation(opacity=0, duration=2)
+        anim += Animation(opacity=1)
+        anim.start(widget)
+
+    def admin(self):
+        print(f"{self.root.ids.Login}")
+        #self.root.ids.slide.load_next(mode="next")
+
+
+if __name__ == "__main__":
+    app = LoginApp()
+    app.run()
+
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Anti-prepaid_meter_bypass
 A software simulation of an anti prepaid meter bypass, to reduce energy theft
+
+ Goal: Build a software that detects when a prepaid meter is bypassed.
+
+Rough Plan:
+    To check if the wires are connected to the prepaid meter, the meter is charged a certain amount of units (made possible by an on-board circuitry)
+which is returned immediately after verification (this will be done multiple times continously). if the verification is unsuccessful then:
+    1. the software will alert the system admin of a suspected by-pass
+    2. send the address of the user who has been suspected of the by-pass.
+
+problems with this system:
+1. What happens when there's no unit left in the meter? wouldn't that give a false alert?
+
+solution
+1. There should be a fixed amount of unit that will be allocated to each prepaid, this unit will only be used for the verification process not for 
+   supply of electricity to the customer 
